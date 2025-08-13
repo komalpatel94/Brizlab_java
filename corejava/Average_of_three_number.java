9452c71b060c8c83da4b9f68426b74703f50c1a3
komalpatel94 <komal.patel_cs23@gla.ac.in>
2025-08-13 10:59:18 +0530

    Create Average_of_three_number.java

---
+++ corejava/Average_of_three_number.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+   public class Main{
+     public static void main(String[] args){
+          Scanner sc = new Scanner(System.in);
+          System.out.println("Enter the num1:");
+          int num1=sc.nextInt();
+          System.out.println("Enter the num2:");
+          int num2=sc.nextInt();
+          System.out.println("Enter the num3:");
+          int num3=sc.nextInt();
+          double avg= (num1+num2 + num3)/3;
+          System.out.println("The Power calculation is:"+ avg);
+}
+   }
+  
