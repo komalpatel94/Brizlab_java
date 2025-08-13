6f856afbd15eb47f57318394d7c06e25119154f2
komalpatel94 <komal.patel_cs23@gla.ac.in>
2025-08-13 11:40:46 +0530

    Update Student_pass_fail_chek.java

--- class_and_object/Student_pass_fail_chek.java
+++ class_and_object/Student_pass_fail_chek.java
@@ -1,1 +1,24 @@
+class Student {
+    String name;
+    int marks;
 
+    Student(String name, int marks) {
+        this.name = name;
+        this.marks = marks;
+    }
+
+    void checkResult() {
+        if (marks >= 40)
+            System.out.println(name + " Passed");
+        else
+            System.out.println(name + " Failed");
+    }
+
+    public static void main(String[] args) {
+        Student s1 = new Student("Komal", 55);
+        Student s2 = new Student("avni", 35);
+
+        s1.checkResult();
+        s2.checkResult();
+    }
+}
