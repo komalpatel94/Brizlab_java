03282364138f9664d084e0c48cefc5c3b3f3f821
komalpatel94 <komal.patel_cs23@gla.ac.in>
2025-08-13 11:42:06 +0530

    Update Local_and_instance_variable_with_same_name.java

--- class_and_object/Local_and_instance_variable_with_same_name.java
+++ class_and_object/Local_and_instance_variable_with_same_name.java
@@ -1,1 +1,13 @@
+class TestThis {
+    int value = 10;
 
+    void showValue() {
+        int value = 20; 
+        System.out.println("Local: " + value);
+        System.out.println("Instance: " + this.value);
+    }
+
+    public static void main(String[] args) {
+        new TestThis().showValue();
+    }
+}
