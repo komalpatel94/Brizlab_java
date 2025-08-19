898c156b41c1e0219e411879aad985aba366da00
komalpatel94 <komal.patel_cs23@gla.ac.in>
2025-08-19 10:10:37 +0530

    Create Create a class that uses all three types of variables: Instance variable.java

---
+++ class_and_object/Create a class that uses all three types of variables: Instance variable.java
@@ -0,0 +1,17 @@
+package Classes;
+
+public class VariableTypes {
+    int instanceVar = 10;       
+    static int staticVar = 20;  
+    void display() {
+        int localVar = 30;      
+        System.out.println("Instance Variable: " + instanceVar);
+        System.out.println("Static Variable: " + staticVar);
+        System.out.println("Local Variable: " + localVar);
+    }
+
+    public static void main(String[] args) {
+        VariableTypes obj = new VariableTypes();
+        obj.display();
+    }
+}
